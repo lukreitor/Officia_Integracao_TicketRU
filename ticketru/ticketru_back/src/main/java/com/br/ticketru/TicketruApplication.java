ca19748e0b5d83af3c1a8bd31c655fe4d2546751
Lucas Martins <lucassena@alunos.utfpr.edu.br>
2024-04-11 12:26:38 -0300

    projeto_backend base
    configuração inicial do projeto

---
+++ ticketru/ticketru_back/src/main/java/com/br/ticketru/TicketruApplication.java
@@ -0,0 +1,13 @@
+package com.br.ticketru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketruApplication.class, args);
+	}
+
+}
---
+++ ticketru/ticketru_back/src/test/java/com/br/ticketru/TicketruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ticketru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
